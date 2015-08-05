d239f54c1a16ee144c8f782d8a0036fba5b39903
AkyunaAkish <akyunaakish@gmail.com>
2015-08-05 12:38:58 -0600

    experiment

---
+++ app.js
@@ -0,0 +1,11 @@
+https://floating-mountain-7407.herokuapp.com/
+
+//axios.get('http://localhost:3000/api/planets') // your API domain
+//    .then(function (response) {
+//        console.log(response);
+//    });
+
+    axios.get('https://floating-mountain-7407.herokuapp.com/api/planets') // your API domain
+        .then(function (response) {
+            console.log(response);
+        });
